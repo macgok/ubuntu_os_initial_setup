ed71f5ed963b87095b607dcb09af387ed478b8b9
Gokul Kannan Sadasivam <70833960+macgok@users.noreply.github.com>
2025-01-19 17:06:41 +0530

    Update Terminal_settings.sh
    Added code for changing background and foreground color using command line

--- Terminal_settings.sh
+++ Terminal_settings.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo 'PS1="\n\[\033[01;34m\]\w \n\[\033[00m\]\$?:~$"' >> ~/.bashrc	# directory \n exit status$
+echo "echo -e '\e]11;rgb:f5/f5/f5\a'" >> ~/.bashrc  # change background color to white  
+echo "echo -e '\e]10;rgb:00/00/00\a'" >> ~/.bashrc  # change foreground (text) color to black
 
 touch ~/.inputrc
 echo '# mappings to have up and down arrow searching through history:' >> ~/.inputrc
